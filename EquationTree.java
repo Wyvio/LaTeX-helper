3d7e85d5416d492cc88506080cfe2313225216dd
J <20728826+Wyvio@users.noreply.github.com>
2021-12-04 20:18:43 -0800

    Started equation tree implementation

---
+++ EquationTree.java
@@ -0,0 +1,15 @@
+public class EquationTree {
+    public static void main(String[] args) {
+
+    }
+
+    private static class EquationNode {
+        public String value;
+        public EquationNode left;
+        public EquationNode right;
+
+        public EquationNode(String value) {
+            this.value = value;
+        }
+    }
+}
