d1ea28ea21cdf3dc04ee32a293b0779fe3893110
Wyvio <20728826+Wyvio@users.noreply.github.com>
2021-12-21 20:14:38 -0800

    implmented reading parens into tree

--- EquationTree.java
+++ EquationTree.java
@@ -10,10 +10,10 @@ public class EquationTree {
     public EquationTree() {
         this(new EquationNode(""));
     }
-    
+
     /**
      * Makes a tree with a specified root
-     * 
+     *
      * @param overallRoot
      */
     public EquationTree(EquationNode overallRoot) {
@@ -28,47 +28,66 @@ public class EquationTree {
 
     /**
      * Makes an equation tree from a string input
-     * 
+     *
      * @param input
      */
     public void makeTree(String input) {
         this.overallRoot = makeTreeHelper(input);
     }
-    
+
     /**
      * makes tree :))
-     * 
+     *
      * @param input
      * @return
      */
     private EquationNode makeTreeHelper(String input) {
+        input = input.trim();
         if (input.isEmpty()) {
             return null;
+        } else if (input.charAt(0) == '(' && input.charAt(input.length() - 1) == ')') {
+            EquationNode root = new EquationNode("(");
+            root.right = new EquationNode(")");
+            root.left = makeTreeHelper(input.substring(1, input.length() - 1));
+            return root;
         } else {
             EquationNode root = new EquationNode("");
+            boolean isSplit = false;
+            int numRightParentheses = 0;
             for (String operator : operators) {
-                if (input.contains(operator)) {                    
-                    int rootIndex = input.indexOf(operator);
-                    root = new EquationNode(operator);
-                    root.left = makeTreeHelper(input.substring(0, rootIndex));
-                    root.right = makeTreeHelper(input.substring(rootIndex + 1));                    
-                    break;
+                if (input.contains(operator)) {
+                    for (int i = 0; i < input.length(); i++) {
+                        String c = "" + input.charAt(i);
+                        if (c.equals("(")) {
+                            numRightParentheses++;
+                        } else if (c.equals(")")) {
+                            numRightParentheses--;
+                        } else if (c.equals(operator) && numRightParentheses == 0) {
+                            root = new EquationNode(operator);
+                            root.left = makeTreeHelper(input.substring(0, i));
+                            root.right = makeTreeHelper(input.substring(i + 1));
+                            isSplit = true;
+                            break;
+                        }
+                    }
                 } else {
                     root = new EquationNode(input);
                 }
+                if (isSplit) {
+                    break;
+                }
             }
-            
             return root;
         }
     }
-    
+
     /**
-     * Prints out LaTeX form 
+     * Prints out LaTeX form
      */
     public String toString() {
         List<String> result = new ArrayList<>();
         toStringHelper(result, overallRoot);
-        
+
         String finalResult = "";
         for (String s : result) {
             finalResult += s;
@@ -92,7 +111,7 @@ public class EquationTree {
             //     result.add("}");
             } else {
                 toStringHelper(result, root.left);
-                result.add(root.value); 
+                result.add(root.value);
                 toStringHelper(result, root.right);
             }
         }
